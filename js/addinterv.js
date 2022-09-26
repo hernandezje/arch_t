0aa3467cc222d3069af381eb406da08a505d6d67
jhernandez <hernandezje366@gmail.com>
2022-09-26 17:50:43 -0300

    correcciones

--- js/addinterv.js
+++ js/addinterv.js
@@ -1,5 +1,6 @@
 document.getElementById("agregar").addEventListener("click", agregar);
     const element = ([]);
+    
     // i=0;
     //const interv=[];
     function agregar(){
@@ -20,15 +21,55 @@ document.getElementById("agregar").addEventListener("click", agregar);
 document.getElementById("ordenar").addEventListener("click", sumIntervalos);
     
 function sumIntervalos() {
-    const ranges = new Set();
-    
-    element.forEach(([start, end]) => {
-        for (let i = start; i < end; i++) ranges.add(i);
-    });
 
+        //primero ordenamos los intervalos con el metodo burbuja
+        for(i=0;i<(element.length-1);i++){
+        
+             for(j=0;j<(element.length-i);j++){
+                
+                 if(element[j]>element[j+1]){
+                     
+                     aux=element[j];
+                   
+                     element[j]=element[j+1];
+                
+                     element[j+1]=aux;
+         
+                }
+            }
+        }
+        
 
-    document.getElementById("result").innerHTML = "La suma de los intervalos ingresados es " + ranges.size;
-    //alert(ranges.size);
+        //sumamos los intervalos sin sumar lo superpuesto
+
+        const ranges = new Set();
+        var aux = 1;
+        element.forEach(([start, end]) => {
+            
+            //si el primer numero del intervalo es mayor que el último del intervalo anterior
+            // recorrer y contar desde el primer numero del intervalo actual
+            if (start > aux) {
+                 
+                for (let i = start; i < parseInt(end); i++){
+                    ranges.add(i);
+                }              
+                         
+            }
+            //si el primer numero del intervalo no es mayor que el último del intervalo anterior (superposicion)
+            // recorrer y contar desde el ultimo numero del intervalo anterior
+            else {
+
+                for (let i = aux; i < parseInt(end); i++){
+                    ranges.add(i);
+                } 
+            }
+            aux=end;
+            
+        });
+
+        document.getElementById("result").innerHTML = "La suma  es " + ranges.size;
+
+        
    
 }
     
--- js/calcular.js
+++ js/calcular.js
@@ -10,19 +10,31 @@ function validate() {
   //alert(resp);
 };
 
+// se resuelve usando la teoria del Maximo Común Divisor, pero usando siempre el mismo divisor.
+// por ejemplo: 
+// 8|2
+// 4|2
+// 2|2
+// 1|
+
 function espotencia(n) {
-  // $("#result").text("is ok");
- 
+  
   var tempN = n;
   var j = 0;
   var upperBound = Math.trunc(Math.sqrt(n));
 
+    //probamos como divisor de n cada numero natural {2,3,4,5...}, hasta upperBound veceses.
+    
     for(var i = 2; i <= upperBound; i++) {
+
+      //dividios n(tempN) mientras i se divisor (resto = 0) 
       while(tempN % i == 0 && tempN > 1) {
         tempN = tempN/i;
         j++;
 
       }
+
+      // si el calculo del M.C.D termina en 1 significa que es potencia perfecta
       if(tempN == 1) {
        
         return resp ="ES POTENCIA PERFECTA! <br> donde "+i+"^"+j+" = "+n;
@@ -33,6 +45,7 @@ function espotencia(n) {
       j = 0;
       
     }
+    // si al terminar de probar el calculo del M.C.D ninguno termina en 1 significa que NO es potencia perfecta
     return resp ="NO ES POTENCIA PERFECTA!";
     //alert("no es potencia perfecta");
   
